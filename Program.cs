d4630a3f9901eead44d2b2541d51016ac13c107a
Tahir Abbas <unaeem@xavor.com>
2024-01-15 14:51:33 +0500

    test commit2

--- Program.cs
+++ Program.cs
@@ -1,25 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebApi.Authorization;
+using WebApi.Helpers;
+using WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+// add services to DI container
+{
+    var services = builder.Services;
+    var env = builder.Environment;
+ 
+    services.AddDbContext<DataContext>();
+    services.AddCors();
+    services.AddControllers().AddJsonOptions(x => 
+    {
+        // serialize enums as strings in api responses (e.g. Role)
+        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    services.AddSwaggerGen();
+
+    // configure strongly typed settings object
+    services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+    // configure DI for application services
+    services.AddScoped<IJwtUtils, JwtUtils>();
+    services.AddScoped<IAccountService, AccountService>();
+    services.AddScoped<IEmailService, EmailService>();
+}
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// migrate any database changes on startup (includes initial db creation)
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();    
+    dataContext.Database.Migrate();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+// configure HTTP request pipeline
+{
+    // generated swagger json and swagger ui middleware
+    app.UseSwagger();
+    app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", ".NET Sign-up and Verification API"));
 
-app.UseRouting();
+    // global cors policy
+    app.UseCors(x => x
+        .SetIsOriginAllowed(origin => true)
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials());
 
-app.UseAuthorization();
+    // global error handler
+    app.UseMiddleware<ErrorHandlerMiddleware>();
 
-app.MapRazorPages();
+    // custom jwt auth middleware
+    app.UseMiddleware<JwtMiddleware>();
 
-app.Run();
+    app.MapControllers();
+}
+
+app.Run("http://localhost:8080");
